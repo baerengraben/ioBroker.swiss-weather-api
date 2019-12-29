ed1776cb6e68eab1cd57c5ee4205700e2b224185
baerengraben <w8kR)056]32o>
2019-12-30 00:53:03 +0100

    Initial Commit

--- admin/words.js
+++ admin/words.js
@@ -14,28 +14,12 @@ systemDictionary = {
 		"pl": "Ustawienia adaptera dla swiss-weather-api",
 		"zh-cn": "swiss-weather-api的适配器设置"
 	},
-	"option1": {
-		"en": "option1",
-		"de": "Option 1",
-		"ru": "Опция 1",
-		"pt": "Opção 1",
-		"nl": "Optie 1",
-		"fr": "Option 1",
-		"it": "opzione 1",
-		"es": "Opción 1",
-		"pl": "opcja 1",
-		"zh-cn": "选项1"
+	"App_Name": {
+		"en": "Name of SRGSSR App",
+		"de": "Name of SRGSSR App"
 	},
-	"option2": {
-		"en": "option2",
-		"de": "Option 2",
-		"ru": "option2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
+	"Base64_ConsumerKey_ConsumerSecret": {
+		"en": "ConsumerKey and ConsumerSecret in Base64",
+		"de": "ConsumerKey and ConsumerSecret in Base64"
 	}
 };
