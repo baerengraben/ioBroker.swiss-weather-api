631711bdc1e1029175814b64a8b359aad3d96c59
baerengraben <w8kR)056]32o>
2019-12-30 00:10:55 +0100

    Initial Commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"swiss-weather-api adapter settings": {
+		"en": "Adapter settings for swiss-weather-api",
+		"de": "Adaptereinstellungen für swiss-weather-api",
+		"ru": "Настройки адаптера для swiss-weather-api",
+		"pt": "Configurações do adaptador para swiss-weather-api",
+		"nl": "Adapterinstellingen voor swiss-weather-api",
+		"fr": "Paramètres d'adaptateur pour swiss-weather-api",
+		"it": "Impostazioni dell'adattatore per swiss-weather-api",
+		"es": "Ajustes del adaptador para swiss-weather-api",
+		"pl": "Ustawienia adaptera dla swiss-weather-api",
+		"zh-cn": "swiss-weather-api的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
