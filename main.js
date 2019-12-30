9c508e2156a3c41069e4682c609b2b1f3feaaf2c
baerengraben <w8kR)056]32o>
2019-12-30 01:35:35 +0100

    Initial Commit

--- main.js
+++ main.js
@@ -36,8 +36,8 @@ class SwissWeatherApi extends utils.Adapter {
 
 		// The adapters config (in the instance object everything under the attribute "native") is accessible via
 		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
+		this.log.info("config option1: " + this.config.App_Name);
+		this.log.info("config option2: " + this.config.Base64_ConsumerKey_ConsumerSecret);
 
 		/*
 		For every state in the system there has to be also an object of type state
