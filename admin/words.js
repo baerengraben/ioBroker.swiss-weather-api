d581aa8e61ddb5a0a8ddb9602cf065846f6435b3
baerengraben <w8kR)056]32o>
2020-03-11 20:40:07 +0100

    - Übersetzungen

--- admin/words.js
+++ admin/words.js
@@ -14,24 +14,64 @@ systemDictionary = {
 		"pl": "Ustawienia adaptera dla swiss-weather-api",
 		"zh-cn": "swiss-weather-api的适配器设置"
 	},
-	"App_Name": {
+	"Name of SRGSSR App": {
 		"en": "Name of SRGSSR App",
-		"de": "Name of SRGSSR App"
+		"de": "Name der SRGSSR App",
+		"ru": "Название приложения СРГССР",
+		"pt": "Nome do aplicativo SRGSSR",
+		"nl": "Naam van de SRGSSR-app",
+		"fr": "Nom de l'application SRGSSR",
+		"it": "Nome dell'app SRGSSR",
+		"es": "Nombre de la aplicación SRGSSR",
+		"pl": "Nazwa aplikacji SRGSSR",
+		"zh-cn": "SRGSSR应用程序的名称"
 	},
 	"ConsumerKey": {
-		"en": "ConsumerKey of App",
-		"de": "ConsumerKey der App"
+	"en": "ConsumerKey of App",
+	"de": "ConsumerKey der App",
+	"ru": "ConsumerKey из приложения",
+	"pt": "ConsumerKey of App",
+	"nl": "ConsumerKey van App",
+	"fr": "ConsumerKey of App",
+	"it": "ConsumerKey of App",
+	"es": "ConsumerKey de la aplicación",
+	"pl": "ConsumerKey of App",
+	"zh-cn": "应用程序的ConsumerKey"
 	},
-	"ConsumerSecret": {
+	"ConsumerSecret":  {
 		"en": "ConsumerSecret of App",
-		"de": "ConsumerSecret der App"
+		"de": "ConsumerSecret von App",
+		"ru": "ConsumerSecret of App",
+		"pt": "ConsumerSecret of App",
+		"nl": "ConsumerSecret van App",
+		"fr": "ConsumerSecret of App",
+		"it": "ConsumerSecret of App",
+		"es": "ConsumerSecret de la aplicación",
+		"pl": "ConsumerSecret of App",
+		"zh-cn": "App的消费者秘方"
 	},
-	"Latitude": {
+	"Latitude":{
 		"en": "Latitude",
-		"de": "Latitude"
+		"de": "Latitude",
+		"ru": "широта",
+		"pt": "Latitude",
+		"nl": "Breedtegraad",
+		"fr": "Latitude",
+		"it": "Latitudine",
+		"es": "Latitud",
+		"pl": "Szerokość",
+		"zh-cn": "纬度"
 	},
 	"Longitude": {
 		"en": "Longitude",
-		"de": "Longitude"
+		"de": "Longitude",
+		"ru": "Долгота",
+		"pt": "Longitude",
+		"nl": "Lengtegraad",
+		"fr": "Longitude",
+		"it": "Longitudine",
+		"es": "Longitud",
+		"pl": "Długość geograficzna",
+		"zh-cn": "经度"
 	}
 };
