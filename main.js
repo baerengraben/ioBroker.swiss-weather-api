c5311b6b891c0b293b6336ecec03d5ffd7f7a3a8
baerengraben <w8kR)056]32o>
2021-05-21 17:24:42 +0200

    https://github.com/baerengraben/ioBroker.swiss-weather-api/issues/32

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ function doIt(self) {
 	//Resolves https://github.com/baerengraben/ioBroker.swiss-weather-api/issues/32
 	dns.resolve4('api.srgssr.ch', function (err, addresses) {
 		if (err) {
-			self.log.error('DNS Resolve Failed for api.srgssr.ch: ' + err.message);
+			self.log.error('DNS Resolve Failed for api.srgssr.ch: ' + err.message + ' Is there an internet connection?');
 			self.log.error('Retrying in 10min...');
 			//todo: set Status of adapter to yellow
 			setTimeout(doIt, 10 * 60000, self);
