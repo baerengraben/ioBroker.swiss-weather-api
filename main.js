16ca69e65a0afe576d5e99f53d06791d49bff433
baerengraben <w8kR)056]32o>
2022-01-04 16:02:54 +0100

    fixing https://github.com/baerengraben/ioBroker.swiss-weather-api/issues/52

--- main.js
+++ main.js
@@ -765,23 +765,20 @@ function setCurrentHour(self){
 
 			//Fix for https://github.com/baerengraben/ioBroker.swiss-weather-api/issues/52
  			var path = self.namespace + ".forecast.60minutes.day0";
-		 	var forecastDate = new Date();
 			if (date.getHours()===0){
 				//check if forecast values already has nextday values in day0/0000
 				self.getState(self.namespace + ".forecast.60minutes.day0.0000.local_date_time", function (err, state) {
 					if ((typeof state !== "undefined") && (state !== null)) {
-						forecastDate = new Date(state.val);
+						if (date.getDay() === new Date(state.val).getDay()){
+							self.log.log("forecast is containing aktual values vor 00:00. So using day0 values");
+						} else {
+							self.log.log("forecast is containing yesterdays values vor 00:00. So using day1 values.");
+							path = self.namespace + ".forecast.60minutes.day1"
+						}
 					} else {
 						self.log.error("Cannot read local_date_time" + ':' + 'This should not happen. Please go to github and create an issue.');
 					}
 				});
-
-				if (date.getDay() === forecastDate.getDay()){
-					self.log.log("forecast is containing aktual values vor 00:00. So using day0 values");
-				} else {
-					self.log.log("forecast is containing yesterdays values vor 00:00. So using day1 values.");
-					path = self.namespace + ".forecast.60minutes.day1"
-				}
 			}
 
 			let currentHourVariables = {};
