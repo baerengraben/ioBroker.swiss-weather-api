b0475acfe06f71b78f3177c4fb28a46451d69a44
baerengraben.intelli.ch <baerengraben.intelli.ch>
2024-10-29 12:11:01 +0100

    Update Testfiles

--- test/package.js
+++ test/package.js
@@ -1,5 +1,5 @@
-const path = import("path");
-const { tests } = import("@iobroker/testing");
+const path = require("path");
+const { tests } = require("@iobroker/testing");
 
 // Validate the package files
 tests.packageFiles(path.join(__dirname, ".."));
