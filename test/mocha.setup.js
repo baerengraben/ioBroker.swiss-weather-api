d1cd8f7691da4eb76cffdb9cb09c033ec9dcd6e5
baerengraben.intelli.ch <baerengraben.intelli.ch>
2024-10-29 17:51:43 +0100

    Update Testfiles

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -5,10 +5,10 @@ process.on("unhandledRejection", (e) => {
 
 // enable the should interface with sinon
 // and load chai-as-promised and sinon-chai by default
-const sinonChai = import("sinon-chai");
-const chaiAsPromised = import("chai-as-promised");
-const { should, use } = import("chai");
+const sinonChai = require("sinon-chai");
+const chaiAsPromised = require("chai-as-promised");
+const { should, use } = require("chai");
 
-//should();
-//use(sinonChai);
-//use(chaiAsPromised);
+should();
+use(sinonChai);
+use(chaiAsPromised);
